462022a2e813eb9d2248054de90e99fadfcf2daa
Cezary Statkiewicz <cezio@thelirium.net>
2017-02-22 15:31:07 +0100

    updated urls

--- geonode/contrib/risks/urls.py
+++ geonode/contrib/risks/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls import url
 from geonode.contrib.risks import views
 
 urlpatterns = [
-    url(r'/cost_benefit/$', views.cost_benefit_index, name='risks_cost_benefit_index'),
-    url(r'/risk_data_extraction/$', views.risk_data_extraction_index, name='risks_risk_data_extraction_index'),
+    url(r'cost_benefit/$', views.cost_benefit_index, name='risks_cost_benefit_index'),
+    url(r'risk_data_extraction/$', views.risk_data_extraction_index, name='risks_risk_data_extraction_index'),
 ]
