ea47dd2bd6fd0768ef6422b8eb7a5bea43f97e92
afabiani <alessio.fabiani@geo-solutions.it>
2017-03-06 15:45:49 +0100

    - Travis Build Fix

--- geonode/contrib/risks/tests/smoke.py
+++ geonode/contrib/risks/tests/smoke.py
@@ -25,7 +25,9 @@ import StringIO
 from django.test import TestCase
 from django.core.management import call_command
 
-from geonode.layers.models import Layer, Style
+from geonode.utils import designals, resignals
+
+from geonode.layers.models import Layer
 from geonode.contrib.risks.models import RiskAnalysis, HazardType
 from geonode.contrib.risks.models import AnalysisType, DymensionInfo
 from geonode.contrib.risks.models import RiskAnalysisDymensionInfoAssociation
@@ -33,6 +35,10 @@ from geonode.contrib.risks.models import RiskAnalysisDymensionInfoAssociation
 TESTDATA_FILE_INI = os.path.join(
     os.path.dirname(__file__),
     'resources/impact_analysis_results_test.ini')
+
+TESTDATA_FILE_DATA = os.path.join(
+    os.path.dirname(__file__),
+    'resources/impact_analysis_results_test.xlsx')
 
 
 class RisksSmokeTests(TestCase):
@@ -50,20 +56,69 @@ class RisksSmokeTests(TestCase):
         '002_risks_hazards',
         '003_risks_analysis',
         '004_risks_dymension_infos',
-        '005_risks_test_base',
-        '005_risks_test_layer'
+        '005_risks_test_base'
     ]
+
+    def setUp(self):
+        # Test initialization
+        designals()
+        call_command('loaddata', '005_risks_test_layer')
+
+        # Sanity Checks
+        self.assertTrue(os.path.isfile(TESTDATA_FILE_INI))
+
+    def tearDown(self):
+        # Test deinit
+        resignals()
 
     def test_smoke_createanalysis(self):
         """Test model here"""
-        self.assertTrue(os.path.isfile(TESTDATA_FILE_INI))
-        out = StringIO.StringIO()
-        call_command('createriskanalysis',
-                     descriptor_file=TESTDATA_FILE_INI, stdout=out)
-        value = out.getvalue()
         try:
-            risk = RiskAnalysis.objects.get(name=str(value).strip())
-            self.assertIsNotNone(risk)
+            hazard = HazardType.objects.get(mnemonic="EQ")
+            self.assertIsNotNone(hazard)
+
+            analysis = AnalysisType.objects.get(name="impact")
+            self.assertIsNotNone(analysis)
+
+            layer = Layer.objects.get(name="test")
+            self.assertIsNotNone(layer)
         except:
             traceback.print_exc()
             self.assertTrue(False)
+
+            out = StringIO.StringIO()
+            call_command('createriskanalysis',
+            descriptor_file=TESTDATA_FILE_INI, stdout=out)
+            value = out.getvalue()
+            try:
+                risk = RiskAnalysis.objects.get(name=str(value).strip())
+                self.assertIsNotNone(risk)
+
+                dim1 = DymensionInfo.objects.get(name="Scenario")
+                self.assertIsNotNone(dim1)
+
+                dim2 = DymensionInfo.objects.get(name="Round Period")
+                self.assertIsNotNone(dim2)
+
+                rd1 = RiskAnalysisDymensionInfoAssociation.objects.filter(dymensioninfo=dim1, riskanalysis=risk)
+                self.assertIsNotNone(rd1)
+                self.assertEqual(len(rd1), 2)
+                self.assertEqual(rd1[0].order, 0)
+                self.assertEqual(rd1[0].axis, u'x')
+                self.assertEqual(rd1[0].value, u'Hospital')
+                self.assertEqual(rd1[1].order, 1)
+                self.assertEqual(rd1[1].axis, u'x')
+                self.assertEqual(rd1[1].value, u'SSP1')
+
+                rd2 = RiskAnalysisDymensionInfoAssociation.objects.filter(dymensioninfo=dim2, riskanalysis=risk)
+                self.assertIsNotNone(rd2)
+                self.assertEqual(len(rd2), 2)
+                self.assertEqual(rd2[0].order, 0)
+                self.assertEqual(rd2[0].axis, u'y')
+                self.assertEqual(rd2[0].value, u'10')
+                self.assertEqual(rd2[1].order, 1)
+                self.assertEqual(rd2[1].axis, u'y')
+                self.assertEqual(rd2[1].value, u'20')
+            except:
+                traceback.print_exc()
+                self.assertTrue(False)
