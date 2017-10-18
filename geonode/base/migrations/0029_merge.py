f187e6421154021f73c20bc22b8c795ef52d3544
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2017-10-18 14:33:58 +0200

    - Group Managers Approval

---
+++ geonode/base/migrations/0029_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('base', '0028_resourcebase_is_approved'),
+        ('base', '0028_auto_20171005_1857'),
+    ]
+
+    operations = [
+    ]
