4446424452f4e6bb6b6c02d19d444f19c855b491
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2017-11-24 16:52:56 +0100

    46-wp7-b22-users-login-with-linkedin-or-facebook

--- geonode/people/models.py
+++ geonode/people/models.py
@@ -23,7 +23,6 @@ from django.utils.translation import ugettext as _
 from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import AbstractUser, UserManager
-from django.contrib import auth
 from django.db.models import signals
 from django.conf import settings
 
@@ -31,11 +30,12 @@ from taggit.managers import TaggableManager
 
 from geonode.base.enumerations import COUNTRIES
 from geonode.groups.models import GroupProfile
-from geonode.notifications_helper import has_notifications, send_notification
-
-from account.models import EmailAddress
+from geonode.notifications_helper import send_notification
+# from account.models import EmailAddress
 
 from .utils import format_address
+from .languages import LANGUAGES
+from .timezones import TIMEZONES
 
 
 email_subject = """IHP-WINS Notifications/Notifications IHP-WINS"""
@@ -82,7 +82,11 @@ class Profile(AbstractUser):
         blank=True,
         null=True,
         help_text=_('name of the responsible organization'))
-    profile = models.TextField(_('Profile'), null=True, blank=True, help_text=_('introduce yourself'))
+    profile = models.TextField(
+        _('Profile'),
+        null=True,
+        blank=True,
+        help_text=_('introduce yourself'))
     position = models.CharField(
         _('Position Name'),
         max_length=255,
@@ -126,6 +130,22 @@ class Profile(AbstractUser):
     keywords = TaggableManager(_('keywords'), blank=True, help_text=_(
         'commonly used word(s) or formalised word(s) or phrase(s) used to describe the subject \
             (space or comma-separated'))
+    language = models.CharField(
+        _("language"),
+        max_length=10,
+        choices=LANGUAGES,
+        default=settings.LANGUAGE_CODE
+    )
+    timezone = models.CharField(
+        max_length=100,
+        default="",
+        choices=TIMEZONES,
+        blank=True,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(Profile, self).__init__(*args, **kwargs)
+        self._previous_active_state = self.is_active
 
     def get_absolute_url(self):
         return reverse('profile_detail', args=[self.username, ])
@@ -140,7 +160,8 @@ class Profile(AbstractUser):
     USERNAME_FIELD = 'username'
 
     def group_list_public(self):
-        return GroupProfile.objects.exclude(access="private").filter(groupmember__user=self)
+        return GroupProfile.objects.exclude(
+            access="private").filter(groupmember__user=self)
 
     def group_list_all(self):
         return GroupProfile.objects.filter(groupmember__user=self).distinct()
@@ -160,7 +181,8 @@ class Profile(AbstractUser):
     @property
     def name_long(self):
         if self.first_name and self.last_name:
-            return '%s %s (%s)' % (self.first_name, self.last_name, self.username)
+            return '%s %s (%s)' % (self.first_name,
+                                   self.last_name, self.username)
         elif (not self.first_name) and self.last_name:
             return '%s (%s)' % (self.last_name, self.username)
         elif self.first_name and (not self.last_name):
@@ -170,7 +192,19 @@ class Profile(AbstractUser):
 
     @property
     def location(self):
-        return format_address(self.delivery, self.zipcode, self.city, self.area, self.country)
+        return format_address(self.delivery, self.zipcode,
+                              self.city, self.area, self.country)
+
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+        self._notify_account_activated()
+        self._previous_active_state = self.is_active
+
+    def _notify_account_activated(self):
+        """Notify user that its account has been activated by a staff member"""
+        became_active = self.is_active and not self._previous_active_state
+        if became_active and self.last_login is None:
+            send_notification(users=(self,), label="account_active")
 
 
 def get_anonymous_user_instance(Profile):
@@ -188,18 +222,6 @@ def profile_post_save(instance, sender, **kwargs):
     # do not create email, when user-account signup code is in use
     if getattr(instance, '_disable_account_creation', False):
         return
-    # keep in sync Profile email address with Account email address
-    if instance.email not in [u'', '', None] and not kwargs.get('raw', False):
-        address, created = EmailAddress.objects.get_or_create(
-            user=instance, primary=True,
-            defaults={'email': instance.email, 'verified': False})
-        if not created:
-            EmailAddress.objects.filter(user=instance, primary=True).update(email=instance.email)
-
-
-def email_post_save(instance, sender, **kw):
-    if instance.primary:
-        Profile.objects.filter(id=instance.user.pk).update(email=instance.email)
 
 
 def profile_pre_save(instance, sender, **kw):
@@ -231,9 +253,3 @@ def profile_signed_up(user, form, **kwargs):
 
 signals.pre_save.connect(profile_pre_save, sender=Profile)
 signals.post_save.connect(profile_post_save, sender=Profile)
-signals.post_save.connect(email_post_save, sender=EmailAddress)
-
-if has_notifications and 'account' in settings.INSTALLED_APPS and getattr(settings, 'ACCOUNT_APPROVAL_REQUIRED', False):
-    from account import signals as s
-    from account.forms import SignupForm
-    s.user_signed_up.connect(profile_signed_up, sender=SignupForm)
