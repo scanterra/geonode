7cbc99ff229f4637cce001b500d98a95e73c74c7
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2017-11-24 20:19:37 +0100

    Merge

---
+++ geonode/people/migrations/0027_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('people', '0026_auto_20171120_0730'),
+        ('people', '0026_merge'),
+    ]
+
+    operations = [
+    ]
