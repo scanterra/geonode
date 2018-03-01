d30c050c3fd0568daf965e53fc0e6036d1cb247f
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2018-03-01 11:13:13 +0100

    - Fix requirements

--- geonode/services/apps.py
+++ geonode/services/apps.py
@@ -19,10 +19,6 @@
 #########################################################################
 
 from geonode.notifications_helper import NotificationsAppConfigBase
-# from ..layers.models import Layer
-
-# from .models import Service
-# from . import signals as services_signals
 
 
 class ServicesAppConfig(NotificationsAppConfigBase):
@@ -31,7 +27,3 @@ class ServicesAppConfig(NotificationsAppConfigBase):
     def ready(self):
         """Connect relevant signals to their corresponding handlers"""
         super(ServicesAppConfig, self).ready()
-#        signals.post_delete.connect(
-#            services_signals.remove_harvest_job, sender=Layer)
-#        signals.post_save.connect(
-#            services_signals.post_save_service, sender=Service)
