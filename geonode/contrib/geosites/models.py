b09bfe810fa1eeab4e049e6882e36610cb60ce68
afabiani <alessio.fabiani@geo-solutions.it>
2018-04-17 15:06:47 +0200

    - GeoSites test cases

--- geonode/contrib/geosites/models.py
+++ geonode/contrib/geosites/models.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#########################################################################
+# ##############################################################################
 #
 # Copyright (C) 2016 OSGeo
 #
@@ -16,7 +16,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-#########################################################################
+# ##############################################################################
+
+import logging
+import traceback
 
 from django.conf import settings
 from django.contrib.sites.models import Site
@@ -29,6 +32,8 @@ from geonode.groups.models import GroupProfile
 from geonode.layers.models import Layer
 from geonode.maps.models import Map
 from geonode.people.models import Profile
+
+logger = logging.getLogger(__name__)
 
 
 class SiteResources(models.Model):
@@ -72,30 +77,42 @@ def post_save_resource(instance, sender, **kwargs):
     assigned to the current site and to the master site"""
     current_site = Site.objects.get_current()
     master_site = Site.objects.get(id=1)
-    SiteResources.objects.get(site=current_site).resources.add(instance.get_self_resource())
-    SiteResources.objects.get(site=master_site).resources.add(instance.get_self_resource())
+    try:
+        SiteResources.objects.get(site=current_site).resources.add(instance.get_self_resource())
+        SiteResources.objects.get(site=master_site).resources.add(instance.get_self_resource())
+    except SiteResources.DoesNotExist:
+        logger.debug(traceback.format_exc())
 
 
 def post_save_site(instance, sender, **kwargs):
     """Signal to create the SiteResources on site save"""
-    SiteResources.objects.get_or_create(site=instance)
-    SitePeople.objects.get_or_create(site=instance)
-    SiteGroups.objects.get_or_create(site=instance)
+    try:
+        SiteResources.objects.get_or_create(site=instance)
+        SitePeople.objects.get_or_create(site=instance)
+        SiteGroups.objects.get_or_create(site=instance)
+    except:
+        logger.debug(traceback.format_exc())
 
 
 def post_delete_resource(instance, sender, **kwargs):
     """Signal to ensure that on resource delete it get remove from the SiteResources as well"""
     current_site = Site.objects.get_current()
     master_site = Site.objects.get(id=1)
-    SiteResources.objects.get(site=current_site).resources.remove(instance.get_self_resource())
-    SiteResources.objects.get(site=master_site).resources.remove(instance.get_self_resource())
+    try:
+        SiteResources.objects.get(site=current_site).resources.remove(instance.get_self_resource())
+        SiteResources.objects.get(site=master_site).resources.remove(instance.get_self_resource())
+    except SiteResources.DoesNotExist:
+        logger.debug(traceback.format_exc())
 
 
 def post_delete_site(instance, sender, **kwargs):
     """Signal to delete the SiteResources on site delete"""
-    SiteResources.objects.filter(site=instance).delete()
-    SitePeople.objects.filter(site=instance).delete()
-    SiteGroups.objects.filter(site=instance).delete()
+    try:
+        SiteResources.objects.filter(site=instance).delete()
+        SitePeople.objects.filter(site=instance).delete()
+        SiteGroups.objects.filter(site=instance).delete()
+    except:
+        logger.debug(traceback.format_exc())
 
 
 def post_save_profile(instance, sender, **kwargs):
@@ -103,14 +120,20 @@ def post_save_profile(instance, sender, **kwargs):
     assigned to the current site only"""
     if not instance.is_superuser and kwargs['created'] and not kwargs['raw'] and instance.username != 'AnonymousUser':
         current_site = Site.objects.get_current()
-        SitePeople.objects.get(site=current_site).people.add(instance)
+        try:
+            SitePeople.objects.get(site=current_site).people.add(instance)
+        except SitePeople.DoesNotExist:
+            logger.debug(traceback.format_exc())
 
 
 def post_delete_profile(instance, sender, **kwargs):
     """Signal to delete the SitePeople on profile delete"""
     if not instance.is_superuser:
         current_site = Site.objects.get_current()
-        SitePeople.objects.get(site=current_site).people.remove(instance)
+        try:
+            SitePeople.objects.get(site=current_site).people.remove(instance)
+        except SitePeople.DoesNotExist:
+            logger.debug(traceback.format_exc())
 
 
 def post_save_group(instance, sender, **kwargs):
@@ -118,13 +141,19 @@ def post_save_group(instance, sender, **kwargs):
     assigned to the current site only"""
     if kwargs['created'] and not kwargs['raw']:
         current_site = Site.objects.get_current()
-        SiteGroups.objects.get(site=current_site).group.add(instance)
+        try:
+            SiteGroups.objects.get(site=current_site).group.add(instance)
+        except SitePeople.DoesNotExist:
+            logger.debug(traceback.format_exc())
 
 
 def post_delete_group(instance, sender, **kwargs):
     """Signal to delete the SitePeople on profile delete"""
     current_site = Site.objects.get_current()
-    SiteGroups.objects.get(site=current_site).group.remove(instance)
+    try:
+        SiteGroups.objects.get(site=current_site).group.remove(instance)
+    except SitePeople.DoesNotExist:
+        logger.debug(traceback.format_exc())
 
 
 # Django doesn't propagate the signals to the parents so we need to add the listeners on the children
